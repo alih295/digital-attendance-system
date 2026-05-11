d404fab75d3f075beb11a4604861f621ac1aa784
alih295 <hafizalihaider295@gmail.com>
2026-05-11 17:19:02 +0500

    Fix API baseURL in axios instance to include the correct endpoint

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://ali295-digitalattendence.hf.space' || "http://localhost:3000/api" ,
+  baseURL: 'https://ali295-digitalattendence.hf.space/api' || "http://localhost:3000/api" ,
   withCredentials: true
 });
 
